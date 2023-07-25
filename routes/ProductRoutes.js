0ad627ed1a6cd9425166efb30e3c203b709a90b4
Kenzastra <100890561+Kenzastra@users.noreply.github.com>
2023-07-25 14:42:45 +0700

    Add files via upload

---
+++ routes/ProductRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getProducts, getProductsById, saveProduct, deleteProduct, updateProduct } from "../controllers/ProductController.js";
+
+const router = express.Router();
+
+router.get('/products', getProducts);
+router.get('/products/:id', getProductsById);
+router.post('/products', saveProduct);
+router.patch('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
+
+export default router;
